72290878ea4804a088fa70995332295c452f2063
lokesh <vpolisetti2023@my.fit.edu>
2025-07-12 23:58:07 +0530

    Updated controller logic

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -1,6 +1,6 @@
 package com.example.SpringBootWeb1;
 
-
+import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
@@ -12,9 +12,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class HomeController {
 
     @RequestMapping("/")
-    public String home() {
-        System.out.println("Home method called");
+
+    public String home(Model model) {
+
+        model.addAttribute("message", "Welcome to Spring Boot JSP!");
+
         return "index";
+
     }
 
 }
